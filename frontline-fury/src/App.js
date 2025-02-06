5b0ed20349f5681dab5497f69c659acc7effe0c0
Frontline-Fury <thefrontlinefury@gmail.com>
2025-02-06 11:08:47 -0800

    netlify

--- frontline-fury/src/App.js
+++ frontline-fury/src/App.js
@@ -1,6 +1,6 @@
-// import Features from "./components/features/Features";
+
 import Footer from "./components/footer/Footer";
-// import Hero from "./components/hero/Hero";
+
 import Homepage from "./components/homepage/Homepage";
 import Navbar from "./components/navbar/Navbar";
 
@@ -12,8 +12,7 @@ function App() {
 
     <Navbar/>
     <Homepage/>
-    {/* <Hero/> */}
-    {/* <Features/> */}
+    
     <Footer/>
     </div>
   );
